89e43b03aa64d41b3553530d74e04ce448c9eb37
Uuu897 <Nastiushakova@yandex.ru>
2024-02-13 19:42:54 +0700

    первые три задания

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-let product = "Socks";
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite (1).jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
     }
 })
